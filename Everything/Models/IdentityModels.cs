ea766891d5c8671e387019d8963dfe0901fda60c
cromober <cromober@gmail.com>
2016-11-13 10:49:20 -0500

    5461

--- Everything/Models/IdentityModels.cs
+++ Everything/Models/IdentityModels.cs
@@ -7,8 +7,8 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace Everything.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    // Hola,
-    //mas bien loquita
+    // Hola, Alejandro
+    //564654zghj
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
